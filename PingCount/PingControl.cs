1195db8a8efa5fca5cc25911eedc28cdd6ae6f0f
Zoio <zoiosilva@yahoo.com.br>
2017-08-09 18:41:59 -0400

    Melhorado manipulação de threads e respostas. Refatorado também.

---
+++ PingCount/PingControl.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+
+namespace PingCount
+{
+    public class PingControl : IDisposable
+    {
+        private readonly Ping ping = new Ping();
+        private readonly int sleep;
+        private readonly string endereco;
+        private Action Acao;
+
+        private enum Estado
+        {
+            Funcionando,
+            Caido,
+        }
+
+        private static long Success { get; set; }
+        private static long Fail { get; set; }
+        private static Estado Situacao { get; set; }
+        private static bool Cancelling { get; set; }
+        private static bool Cancelled { get; set; }
+
+        public PingControl(int sleep = 750, string endereco = "8.8.8.8")
+        {
+            this.sleep = sleep;
+            this.endereco = endereco;
+
+            Init();
+        }
+
+        private void Init()
+        {
+            ping.PingCompleted += (s, args) =>
+            {
+                if (args.Reply.Status == IPStatus.Success)
+                    Pingou();
+                else
+                    Falhou();
+
+                ShowStatus();
+
+                ((AutoResetEvent)args.UserState).Set();
+            };
+
+            Acao = () =>
+            {
+                using (var waiter = new AutoResetEvent(false))
+                    do
+                    {
+                        try
+                        {
+                            ping.SendAsync(endereco, waiter);
+                            waiter.WaitOne();
+                        }
+                        catch (PingException)
+                        {
+                            Falhou();
+                            ShowStatus();
+                        }
+
+                        Thread.Sleep(sleep);
+                    } while (!Cancelling);
+
+                Cancelled = true;
+            };
+        }
+
+        public void StartAsync()
+        {
+            Cancelling = false;
+            Cancelled = false;
+
+            Acao.BeginInvoke(Acao.EndInvoke, null);
+        }
+
+        public void Stop()
+        {
+            Cancelling = true;
+            while (!Cancelled) ;
+        }
+
+        private static void Pingou()
+        {
+            Success++;
+            if (Situacao == Estado.Caido)
+            {
+                Console.Beep(1318, 180); //E (Mi)
+                Console.Beep(1760, 180); //A (Lá)
+            }
+            Situacao = Estado.Funcionando;
+        }
+
+        private static void Falhou()
+        {
+            Situacao = Estado.Caido;
+            Fail++;
+            Console.Beep(880, 200); //A (Lá)
+        }
+
+        private static void ShowStatus()
+        {
+            decimal all = Fail + Success;
+
+            Console.Clear();
+            Console.WriteLine("Ping status: {0} ({1:N0}%) succeeded, {2} ({3:N0}%) failed somehow.", Success, (Success / all) * 100, Fail, (Fail / all) * 100);
+        }
+
+        public void Dispose()
+        {
+            if (!Cancelling)
+                throw new Exception("Tentativa de liberação de recurso ainda em uso.");
+
+            ping.Dispose();
+        }
+    }
+}
--- PingCount/Program.cs
+++ PingCount/Program.cs
@@ -1,88 +1,21 @@
 ﻿using System;
-using System.Net.NetworkInformation;
-using System.Threading;
 
 namespace PingCount
 {
     class Program
     {
-        private enum Estado
-        {
-            Funcionando,
-            Caido,
-        }
-
-        private static long Success { get; set; }
-        private static long Fail { get; set; }
-        private static Estado Situacao { get; set; }
-
         static void Main(string[] args)
         {
-            //Console.WriteLine("WRAAAAAAAAAAAAAA!!");
-            //Console.ReadKey();
             Console.SetWindowSize(65, 05);
-            Teste();
-        }
 
-        private static void Teste()
-        {
-            using (var waiter = new AutoResetEvent(false))
-            using (var ping = new Ping())
+            using (var pc = new PingControl())
             {
-                ping.PingCompleted += (s, args) =>
-                {
-                    if (args.Reply.Status == IPStatus.Success)
-                        Yay();
-                    else
-                        Ahhh();
+                pc.StartAsync();
 
-                    ShowStatus();
+                Console.ReadKey();
 
-                    ((AutoResetEvent)args.UserState).Set();
-                };
-
-                do
-                {
-                    try
-                    {
-                        ping.SendAsync("8.8.8.8", waiter);
-                        waiter.WaitOne();
-                    }
-                    catch (PingException)
-                    {
-                        Ahhh();
-                        ShowStatus();
-                    }
-
-                    Thread.Sleep(750);
-                } while (true);
+                pc.Stop();
             }
-        }
-
-        private static void Yay()
-        {
-            Success++;
-            if (Situacao == Estado.Caido)
-            {
-                Console.Beep(1318, 180); //E (Mi)
-                Console.Beep(1760, 180); //A (Lá)
-            }
-            Situacao = Estado.Funcionando;
-        }
-
-        private static void Ahhh()
-        {
-            Situacao = Estado.Caido;
-            Fail++;
-            Console.Beep(880, 200); //A (Lá)
-        }
-
-        private static void ShowStatus()
-        {
-            decimal all = Fail + Success;
-
-            Console.Clear();
-            Console.WriteLine("Ping status: {0} ({1:N0}%) succeeded, {2} ({3:N0}%) failed somehow.", Success, (Success / all) * 100, Fail, (Fail / all) * 100);
         }
     }
 }
